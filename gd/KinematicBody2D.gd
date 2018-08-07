cc5f2c8d532c2ef1d9cee85d8795995bf8d6dd79
joshua lambert <joshuakylelambert@gmail.com>
2018-08-07 18:25:44 -0400

    p5,3.01

---
+++ gd/KinematicBody2D.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const ACCELERATION = 50
+const MAX_SPEED = 200
+const JUMP_HEIGHT = -600
+
+var motion = Vector2()
+
+func _process(delta):
+	motion.y += GRAVITY
+	var friction = false
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	if Input.is_action_pressed("ui_right"):
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
+	else:
+		$Sprite.play("Idle")
+		friction = true
+		motion.x = lerp(motion.x, 0, 0.2)
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
+	else:
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
+	
+	motion = move_and_slide(motion, UP)
+	pass
